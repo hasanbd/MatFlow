d699cad8161e04c0b182eacaff8d4fd938f7288b
Mehedi Hasan Arafat <mehedihasanarafat10@gmail.com>
2023-01-18 23:14:27 +0600

    mehedi v3

--- ml.py
+++ ml.py
@@ -11,7 +11,9 @@ st.set_page_config(
     layout="wide",
     page_title="MatFlow: Materials Design System",
     page_icon="👨‍💻",
+    # initial_sidebar_state="collapsed"
 )
+
 # st.markdown("""
 # <h3 class="title" style='text-align: center; background-color: lightblue; font-family: "Lucida Console", "Courier New", monospace;'>
 #         Knowledgebased Materials Design System Using Machine Learning</h3>
@@ -28,25 +30,27 @@ hide_streamlit_style = """
      footer{visibility:hidden}
     </style>
 """
-# st.markdown('''
-# <style>
-# .css-163ttbj {
-#     position: relative;
-#     top: 2px;
-#     background-color: rgb(240, 242, 246);
-#     z-index: 999991;
-#     min-width: 264px;
-#     max-width: 269px;
-#     orm: none;
-#     transition: transform 300ms ease 0s, min-width 300ms ease 0s, max-width 300ms ease 0s;
-# }
-# </style>
-# ''',unsafe_allow_html=True)
+st.markdown('''
+<style>
+.css-163ttbj {
+    position: relative;
+    top: 2px;
+    background-color: rgb(240, 242, 246);
+    z-index: 999991;
+    min-width: 264px;
+    max-width: 269px;
+    orm: none;
+    display:none;
+    transition: transform 300ms ease 0s, min-width 300ms ease 0s, max-width 300ms ease 0s;
+}
+</style>
+''',unsafe_allow_html=True)
 # Remove Streamlit footer note and repository notes
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 #
 # # Add all my application pages here
-# ml.connect("Start", home.load_view)
+ml.connect("Start", home.load_view)
+ml.connect("demo",dl.main)
 # ml.connect("🗃️Upload Dataset", dl.main)
 # ml.connect("⚙️Training", ModelBuild.main)
 # ml.connect("💡 Best Fit", machine_learning.main)
--- selectpage.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-def select():
-    st.write('What you want to do?')
-    col1,col2=st.columns(2)
-    with col1:
-        st.button('Project')
-    with col2:
-        st.button('File')
