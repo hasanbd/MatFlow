63441f89fb890270ef31e84aef991d0c4f16a6ce
hasanbd <hasancsecu@gmail.com>
2023-01-20 23:17:39 +0600

    Update DataExtraction.py

--- code/DataExtraction.py
+++ code/DataExtraction.py
@@ -225,11 +225,11 @@ def dynamocs():
     utils.ShowHistogramCharts(data)
     utils.SaveDataToOutput(data, 'extraction-dyomics')
     utils.LoadDataFromOutput('extraction-dyomics')
-    st.write('hellojdhkhsfk')
 def Photoche():
     st.write('step3')
     # display(HTML("<style>.container { width:100% !important; }</style>"))
-    data = pd.read_csv('./rawData/PhotoChemCAD3/PCAD3 Compd Database 2018/2018_03 PCAD3.db', sep='\t', encoding='oem')
+    data = pd.read_csv('../rawData/PhotoChemCAD3/PCAD3 Compd Database 2018/2018_03 PCAD3.db', sep='\t', encoding='oem')
+    st.write('step2')
     data.drop(['#', 'Instrument', 'Date', 'Reference', 'Inv', 'Instrument.1', 'Date.1', 'Reference.1', 'Inv.1',
                'Unnamed: 21'], axis='columns', inplace=True)
     #data.head(1)
@@ -276,7 +276,6 @@ def pubche():
 
 def run_all():
     deep4che()
-    st.write('success1')
     dynamocs()
     st.write('success2')
     Photoche()
