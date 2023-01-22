46880a98b4eb78b93dd426a27daa5ce8a37284f3
hasanbd <hasancsecu@gmail.com>
2023-01-22 23:30:57 +0600

    Update DataExtraction.py

--- code/DataExtraction.py
+++ code/DataExtraction.py
@@ -266,9 +266,7 @@ def pubche():
     utils.LoadDataFromOutput('extraction-pubChem')
 
 def run_all():
-    
-    st.write('done')
-    deep4che()
+#     deep4che()
 #     dynamocs()
 #     Photoche()
 #     pubche()
